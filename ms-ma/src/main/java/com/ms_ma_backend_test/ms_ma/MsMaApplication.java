608493a728801edb2ef8393ffd1249882c0b2ab5
Pichau <igorof06@outlook.com>
2024-09-15 17:13:01 -0300

    Starting project

---
+++ ms-ma/src/main/java/com/ms_ma_backend_test/ms_ma/MsMaApplication.java
@@ -0,0 +1,13 @@
+package com.ms_ma_backend_test.ms_ma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsMaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsMaApplication.class, args);
+	}
+
+}
---
+++ ms-ma/src/test/java/com/ms_ma_backend_test/ms_ma/MsMaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms_ma_backend_test.ms_ma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsMaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
