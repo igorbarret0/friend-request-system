383408684f327b5fa9e597d67e5166b5679c50d0
Pichau <igorof06@outlook.com>
2024-09-15 20:50:17 -0300

    Creation of UserController: signUp method

---
+++ ms-ma/src/main/java/com/ms_ma_backend_test/ms_ma/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.ms_ma_backend_test.ms_ma.controller;
+
+import com.ms_ma_backend_test.ms_ma.dtos.SignUpRequest;
+import com.ms_ma_backend_test.ms_ma.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<String> signUp(@RequestBody SignUpRequest request) {
+
+        userService.signUp(request);
+        return ResponseEntity.ok("User signed up successfully");
+    }
+}
