c5f43c660608b973acfb52e7f3ffd810e86990c5
Pichau <igorof06@outlook.com>
2024-09-17 01:17:47 -0300

    Added method findByUsername in UserRepository

--- ms-ma/src/main/java/com/ms_ma_backend_test/ms_ma/repository/UserRepository.java
+++ ms-ma/src/main/java/com/ms_ma_backend_test/ms_ma/repository/UserRepository.java
@@ -4,6 +4,11 @@ import com.ms_ma_backend_test.ms_ma.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
 }
